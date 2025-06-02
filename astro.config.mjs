d2b2738a1d7e7e80a5a220c3d3a54f77c6106fa3
JasterV <49537445+JasterV@users.noreply.github.com>
2025-06-02 03:33:13 +0200

    [feat] New release with simplified codebase and design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,16 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jaster.xyz",
+  integrations: [
+    icon({
+      include: {
+        // Include only three `mdi` icons in the bundle
+        // Otherwise, Astro Icons could include every single icon in the mdi package and result in a huge bundle size
+        mdi: ["github", "linkedin"],
+      },
+    }),
+  ],
 });
